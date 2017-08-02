cd49421d370f770ea8916884223cd3494657c797
Maxim Bravo <maxim@alexbravo.com>
2017-08-02 11:26:08 -0700

    Rearrange some inner classes

---
+++ Chongo3/app/src/main/java/com/maximbravo/chongo3/SectionsPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.maximbravo.chongo3;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a TabFragment (defined as a static inner class below).
+        return TabFragment.newInstance(position);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Recent";
+            case 1:
+                return "Word List";
+        }
+        return null;
+    }
+}
